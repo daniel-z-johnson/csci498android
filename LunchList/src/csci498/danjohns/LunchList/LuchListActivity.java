d7d9c2ff19a86fe531546ef36f1db620d5e2ea2c
Daniel Johnson <danjohns@mymail.mines.edu>
2012-09-05 12:36:14 -0600

    Finished ch5 expect 'extra credits', will do later tonight

--- LunchList/src/csci498/danjohns/LunchList/LuchListActivity.java
+++ LunchList/src/csci498/danjohns/LunchList/LuchListActivity.java
@@ -22,111 +22,106 @@ import android.widget.TextView;
 
 public class LuchListActivity extends Activity {
 	List<Restaurant> model = new ArrayList<Restaurant>();
-	RestaurantAdapter adapter=null;
+	RestaurantAdapter adapter = null;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        Button save = (Button)findViewById(R.id.save);
-        
-        save.setOnClickListener(onSave);
-        
-        ListView list = (ListView)findViewById(R.id.restraurants);
-        
-        adapter = new RestaurantAdapter();
-        list.setAdapter(adapter);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
-    private View.OnClickListener onSave = new View.OnClickListener(){
+		Button save = (Button) findViewById(R.id.save);
+
+		save.setOnClickListener(onSave);
+
+		ListView list = (ListView) findViewById(R.id.restraurants);
+
+		adapter = new RestaurantAdapter();
+		list.setAdapter(adapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	private View.OnClickListener onSave = new View.OnClickListener() {
 
 		@Override
 		public void onClick(View v) {
 			Restaurant r = new Restaurant();
-			EditText name = (EditText)findViewById(R.id.name);
-			EditText address = (EditText)findViewById(R.id.addr);
-			
+			EditText name = (EditText) findViewById(R.id.name);
+			EditText address = (EditText) findViewById(R.id.addr);
+
 			r.setName(name.getText().toString());
 			r.setAddress(address.getText().toString());
-			
-			RadioGroup types = (RadioGroup)findViewById(R.id.types);
-			
-			switch(types.getCheckedRadioButtonId()){
-				case R.id.sit_down:
-					r.setType("sit_down");
-					break;
-				
-				case R.id.take_out:
-					r.setType("take_out");
-					break;
-					
-				case R.id.delivery:
-					r.setType("delivery");
-					break;
+
+			RadioGroup types = (RadioGroup) findViewById(R.id.types);
+
+			switch (types.getCheckedRadioButtonId()) {
+			case R.id.sit_down:
+				r.setType("sit_down");
+				break;
+
+			case R.id.take_out:
+				r.setType("take_out");
+				break;
+
+			case R.id.delivery:
+				r.setType("delivery");
+				break;
 			}
-			
+
 			adapter.add(r);
 		}
-    };
-    
-    class RestaurantAdapter extends ArrayAdapter<Restaurant> {
-    	public RestaurantAdapter() {
-			super(LuchListActivity.this,
-					android.R.layout.simple_list_item_1,
-					model);
+	};
+
+	class RestaurantAdapter extends ArrayAdapter<Restaurant> {
+		RestaurantAdapter() {
+			super(LuchListActivity.this, R.layout.row, model);
 		}
-    }
-    
-    public View getView(int position, View convertView, ViewGroup parant){
-    	View row = convertView;
-    	
-    	if( row == null ){
-    		LayoutInflater inflater = getLayoutInflater();
-    		row = inflater.inflate(R.layout.row, null);
-    	}
-    	
-    	Restaurant r = model.get(position);
-    	((TextView)row.findViewById(R.id.title)).setText(r.getName());
-    	((TextView)row.findViewById(R.id.address)).setText(r.getName());
-    	
-    	ImageView icon = (ImageView)row.findViewById(R.id.icon);
-    	
-    	if(r.getType().equals("sit_down"))
-    		icon.setImageResource(R.drawable.ball_red);
-    	else if(r.getType().equals("take_out"))
-    		icon.setImageResource(R.drawable.ball_yellow);
-    	else
-    		icon.setImageResource(R.drawable.ball_green);
-    	return row;
-    }
-    
-    static class RestaurantHolder {
-    	private TextView name = null;
-    	private TextView address = null;
-    	private ImageView icon = null;
-    	
-    	RestaurantHolder(View row){
-    		name = (TextView)row.findViewById(R.id.title);
-    		address = (TextView)row.findViewById(R.id.address);
-    		icon = (ImageView)row.findViewById(R.id.icon);
-    	}
-    	
-    	void populateFrom(Restaurant r){
-    		name.setText(r.getName());
-    		address.setText(r.getAddress());
-    		if(r.getType().equals("sit_down"))
-        		icon.setImageResource(R.drawable.ball_red);
-        	else if(r.getType().equals("take_out"))
-        		icon.setImageResource(R.drawable.ball_yellow);
-        	else
-        		icon.setImageResource(R.drawable.ball_green);
-    	}
-    }
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parant) {
+			View row = convertView;
+			RestaurantHolder holder = null;
+
+			if (row == null) {
+				LayoutInflater inflater = getLayoutInflater();
+
+				row = inflater.inflate(R.layout.row, parant, false);
+				holder = new RestaurantHolder(row);
+				row.setTag(holder);
+			} else
+				holder = (RestaurantHolder) row.getTag();
+
+			holder.populateFrom(model.get(position));
+
+			return row;
+		}
+	}
+
+	static class RestaurantHolder {
+		private TextView name = null;
+		private TextView address = null;
+		private ImageView icon = null;
+
+		RestaurantHolder(View row) {
+			name = (TextView) row.findViewById(R.id.title);
+			address = (TextView) row.findViewById(R.id.address);
+			icon = (ImageView) row.findViewById(R.id.icon);
+		}
+
+		void populateFrom(Restaurant r) {
+			name.setText(r.getName());
+			address.setText(r.getAddress());
+
+			if (r.getType().equals("sit_down"))
+				icon.setImageResource(R.drawable.ball_red);
+			else if (r.getType().equals("take_out"))
+				icon.setImageResource(R.drawable.ball_yellow);
+			else
+				icon.setImageResource(R.drawable.ball_green);
+		}
+	}
 }
