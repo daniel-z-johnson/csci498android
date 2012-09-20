b0481824efad2a9cc561ee1663ba1985140013c5
Daniel Johnson <danjohns@mymail.mines.edu>
2012-09-19 21:38:59 -0600

    completed step 2 of ch11, added implementation of onCreate

--- LunchList/src/csci498/danjohns/LunchList/RestaurantHelper.java
+++ LunchList/src/csci498/danjohns/LunchList/RestaurantHelper.java
@@ -15,14 +15,13 @@ public class RestaurantHelper extends SQLiteOpenHelper{
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
-		
+		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT)");
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
-		
+		//no-op, since will not be called until 2nd schema
+		//version exits
 	}
 
 }
