f9cd2ba918b335e9b9563179d93ca9f328dabf3d
Daniel Johnson <danjohns@mymail.mines.edu>
2012-09-19 21:33:46 -0600

    moved restaurantHelper to the correct package and changed method argument names to something better than the default names

--- LunchList/src/csci498/danjohns/LunchList/RestaurantHelper.java
+++ LunchList/src/csci498/danjohns/LunchList/RestaurantHelper.java
@@ -1,3 +1,4 @@
+package csci498.danjohns.LunchList;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -13,13 +14,13 @@ public class RestaurantHelper extends SQLiteOpenHelper{
 	}
 
 	@Override
-	public void onCreate(SQLiteDatabase arg0) {
+	public void onCreate(SQLiteDatabase db) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub
 		
 	}
