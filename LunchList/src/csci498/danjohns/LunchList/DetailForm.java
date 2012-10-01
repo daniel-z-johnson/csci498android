67ccf0cdf25ba33304a0e1c7b04ac7c216ef3917
Daniel Johnson <danjohns@mymail.mines.edu>
2012-09-30 20:43:04 -0600

    ch14 step3, added onRestoreInstanceState method to detail form

--- LunchList/src/csci498/danjohns/LunchList/DetailForm.java
+++ LunchList/src/csci498/danjohns/LunchList/DetailForm.java
@@ -103,4 +103,14 @@ public class DetailForm extends Activity {
 		state.putString("notes", notes.getText().toString());
 		state.putInt("type", types.getCheckedRadioButtonId());
 	}
+	
+	@Override
+	public void onRestoreInstanceState(Bundle state) {
+		super.onRestoreInstanceState(state);
+		
+		name.setText(state.getString("name"));
+		address.setText(state.getString("address"));
+		notes.setText(state.getString("notes"));
+		types.check(state.getInt("type"));
+	}
 }
