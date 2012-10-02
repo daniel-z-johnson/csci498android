e6c277dfb960d73ff4a8bbfbef2ac176d02b071c
Daniel Johnson <danjohns@mymail.mines.edu>
2012-10-01 19:34:35 -0600

    ch15 step5, added a inner class that extends asyncTask to feedActivity

--- LunchList/src/csci498/danjohns/LunchList/FeedActivity.java
+++ LunchList/src/csci498/danjohns/LunchList/FeedActivity.java
@@ -1,7 +1,17 @@
 package csci498.danjohns.LunchList;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 
 public class FeedActivity extends Activity {
+	
+	private static class FeedTask extends AsyncTask<String, Void, Void> {
 
+		@Override
+		protected Void doInBackground(String... urls) {
+			
+			return null;
+		}
+		
+	}
 }
