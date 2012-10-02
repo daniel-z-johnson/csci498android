88fe949e7df3972ac24fd7156d03f84bcdfb7fef
Daniel Johnson <danjohns@mymail.mines.edu>
2012-10-01 19:51:51 -0600

    Scare, eclipse froze on me for a few seconds and I decided that I needed to push upstream added some methods and member variables to the inner class of feed activity

--- LunchList/src/csci498/danjohns/LunchList/FeedActivity.java
+++ LunchList/src/csci498/danjohns/LunchList/FeedActivity.java
@@ -1,16 +1,49 @@
 package csci498.danjohns.LunchList;
 
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.AsyncTask;
+import android.util.Log;
+import android.app.ListActivity;
 
 public class FeedActivity extends Activity {
 	
 	private static class FeedTask extends AsyncTask<String, Void, Void> {
-
+		private Exception e = null;
+		private FeedActivity activity = null;
+		
 		@Override
 		protected Void doInBackground(String... urls) {
-			
+			try{
+				DefaultHttpClient client = new DefaultHttpClient();
+				HttpGet getMethod = new HttpGet(urls[0]);
+				ResponseHandler<String> responseHandler = new BasicResponseHandler();
+				String responseBody = client.execute(getMethod, responseHandler);
+				Log.d("FeedActivity", responseBody);
+			}catch (Exception e) {
+				this.e = e;
+			}
 			return null;
+		}
+		
+		@Override
+		public void onPostExecute(Void unused) {
+			if (e == null){
+				//TODO hooray
+			}else {
+				Log.e("LunchList", "Exception parsing fedd", e);
+				activity.goBlooey(e);
+			}
+			
+		}
+		
+		private void goBlooey(Throwable t) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		}
 		
 	}
