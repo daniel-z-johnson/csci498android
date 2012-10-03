1ae0c66fda51a193d0ef2cbc192f718c250a8d7a
Daniel Johnson <danjohns@mymail.mines.edu>
2012-10-02 20:53:00 -0600

    ch15 step7, re-did FeedTask to take advantage of jar file

--- LunchList/src/csci498/danjohns/LunchList/FeedActivity.java
+++ LunchList/src/csci498/danjohns/LunchList/FeedActivity.java
@@ -1,9 +1,7 @@
 package csci498.danjohns.LunchList;
 
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -12,49 +10,44 @@ import android.util.Log;
 
 public class FeedActivity extends Activity {
 	
-	private static class FeedTask extends AsyncTask<String, Void, Void> {
+	private static class FeedTask extends AsyncTask<String, Void, RSSFeed> {
+		private RSSReader reader = new RSSReader();
 		private Exception e = null;
 		private FeedActivity activity = null;
 		
-		FeedTask(FeedActivity activity){
+		FeedTask(FeedActivity activity) {
 			attach(activity);
 		}
 		
+		void attach(FeedActivity activity) {
+			this.activity = activity;
+		}
+		
+		void detach() {
+			this.activity = null;
+		}
+		
 		@Override
-		protected Void doInBackground(String... urls) {
-			try{
-				DefaultHttpClient client = new DefaultHttpClient();
-				HttpGet getMethod = new HttpGet(urls[0]);
-				ResponseHandler<String> responseHandler = new BasicResponseHandler();
-				String responseBody = client.execute(getMethod, responseHandler);
-				
-				Log.d("FeedActivity", responseBody);
-			}catch (Exception e) {
+		public RSSFeed doInBackground(String... urls) {
+			RSSFeed result = null;
+			
+			try {
+				result = reader.load(urls[0]);
+			} catch(Exception e) {
 				this.e = e;
 			}
-			return null;
+			
+			return result;
 		}
 		
 		@Override
-		public void onPostExecute(Void unused) {
-			if (e == null){
-				//TODO hooray
-			}else {
-				Log.e("LunchList", "Exception parsing fedd", e);
-				activity.goBlooey(e);
+		public void onPostExecute(RSSFeed feed) {
+			if(e == null)
+				activity.setFeed(feed);
+			else{
+				Log.e("LunchList", "Exception parsing feed", e);
+				//activity.goBlooey(e);
 			}
-			
 		}
-		
-		private void goBlooey(Throwable t) {
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			
-			builder
-				.setTitle("Exception!")
-				.setMessage(t.toString())
-				.setPositiveButton("ok", null)
-				.show();
-		}
-		
 	}
 }
