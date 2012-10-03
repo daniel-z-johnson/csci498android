968eb1f8f9dd1f0669f76993d6de22130bd646dc
Daniel Johnson <danjohns@mymail.mines.edu>
2012-10-02 20:58:20 -0600

    ch15 step8, added bare-bones of class feedAdapter that extends baseAdapter which is located in FeedActivity

--- LunchList/src/csci498/danjohns/LunchList/FeedActivity.java
+++ LunchList/src/csci498/danjohns/LunchList/FeedActivity.java
@@ -7,6 +7,9 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 public class FeedActivity extends Activity {
 	
@@ -49,5 +52,33 @@ public class FeedActivity extends Activity {
 				//activity.goBlooey(e);
 			}
 		}
+	}
+	
+	private class FeedAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
 	}
 }
