d8ca925703bb406866ca012f0790de6b5082866d
Daniel Johnson <danjohns@mymail.mines.edu>
2012-10-02 21:42:38 -0600

    ch15 step8, finished but something may be wrong with my xml

--- LunchList/src/csci498/danjohns/LunchList/FeedActivity.java
+++ LunchList/src/csci498/danjohns/LunchList/FeedActivity.java
@@ -1,17 +1,69 @@
 package csci498.danjohns.LunchList;
 
 import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSItem;
 import org.mcsoxford.rss.RSSReader;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.app.ListActivity;
 
-public class FeedActivity extends Activity {
+public class FeedActivity extends ListActivity {
+	public static final String FEED_URL = "csci498.danjohns.LunchList.FEED_URL";
+	private InstanceState state = null;
+	//Inner classes at end of methods
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		state = (InstanceState)getLastNonConfigurationInstance(); //sigh... again
+		
+		if (state == null) {
+			state = new InstanceState();
+			state.task = new FeedTask(this);
+			state.task.execute(getIntent().getStringArrayExtra(FEED_URL));
+		} else {
+			
+			if (state.task != null)
+				state.task.attach(this);
+			
+			if (state.feed != null)
+				setFeed(state.feed);
+		}
+	}
+	
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		
+		if (state.task != null)
+			state.task.detach();
+		
+		return state;
+		
+	}
+	
+	private void setFeed(RSSFeed feed) {
+		state.feed = feed;
+		setListAdapter(new FeedAdapter(feed));
+	}
+	
+	private void goBlooey(Throwable t) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		
+		builder
+			.setTitle("Exception!")
+			.setMessage(t.toString())
+			.setPositiveButton("ok", null)
+			.show();
+	}
 	
 	private static class FeedTask extends AsyncTask<String, Void, RSSFeed> {
 		private RSSReader reader = new RSSReader();
@@ -47,38 +99,58 @@ public class FeedActivity extends Activity {
 		public void onPostExecute(RSSFeed feed) {
 			if(e == null)
 				activity.setFeed(feed);
-			else{
+			else {
 				Log.e("LunchList", "Exception parsing feed", e);
-				//activity.goBlooey(e);
+				activity.goBlooey(e);
 			}
 		}
 	}
 	
 	private class FeedAdapter extends BaseAdapter {
+		RSSFeed feed = null;
+		
+		FeedAdapter(RSSFeed feed) {
+			super();
+			
+			this.feed = feed;
+		}
 
 		@Override
 		public int getCount() {
-			// TODO Auto-generated method stub
-			return 0;
+			return feed.getItems().size();
 		}
 
 		@Override
 		public Object getItem(int position) {
-			// TODO Auto-generated method stub
-			return null;
+			return feed.getItems().get(position);
 		}
 
 		@Override
 		public long getItemId(int position) {
-			// TODO Auto-generated method stub
-			return 0;
+			return position;
 		}
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			// TODO Auto-generated method stub
-			return null;
+			View row = convertView;
+			
+			if (row == null) {
+				LayoutInflater inflator = getLayoutInflater();
+				
+				row = inflator.inflate(android.R.layout.simple_list_item_1, parent, false);
+			}
+			
+			RSSItem item =(RSSItem)getItem(position);
+			
+			((TextView)row).setText(item.getTitle()); //Lisp anyone?
+			
+			return row;
 		}
-		
+				
+	}
+	
+	private static class InstanceState {
+		RSSFeed feed = null;
+		FeedTask task = null;
 	}
 }
