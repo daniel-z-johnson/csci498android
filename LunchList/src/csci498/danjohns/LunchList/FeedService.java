68cea4261cc224ba1ac292616813f0b131e25502
Daniel Johnson <danjohns@mymail.mines.edu>
2012-10-06 21:41:16 -0600

    ch16 step2, FoodService added implementation to onHandleIntent

--- LunchList/src/csci498/danjohns/LunchList/FeedService.java
+++ LunchList/src/csci498/danjohns/LunchList/FeedService.java
@@ -1,9 +1,14 @@
 package csci498.danjohns.LunchList;
 
+import org.mcsoxford.rss.RSSReader;
+import org.mcsoxford.rss.RSSFeed;
+
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 public class FeedService extends IntentService {
+	public static final String EXTRA_URL = "csci498.danjohns.LunchList.EXTRA_URL";
 
 	public FeedService() {
 		super("FeedService");
@@ -11,7 +16,14 @@ public class FeedService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent i) {
-		// Do Something
+		RSSReader reader = new RSSReader();
+		
+		try {
+			RSSFeed result = reader.load(i.getStringExtra(EXTRA_URL));
+		}
+		catch (Exception e) {
+			Log.e("LunchList", "Exception parsing feed", e);
+		}
 	}
 
 }
