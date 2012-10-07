e28089c162beca7ff87931abc956a8b95b47c540
Daniel Johnson <danjohns@mymail.mines.edu>
2012-10-06 21:20:32 -0600

    ch16 step1, stub program for FoodService

---
+++ LunchList/src/csci498/danjohns/LunchList/FeedService.java
@@ -0,0 +1,17 @@
+package csci498.danjohns.LunchList;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class FeedService extends IntentService {
+
+	public FeedService() {
+		super("FeedService");
+	}
+
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+		// Do Something
+	}
+
+}
