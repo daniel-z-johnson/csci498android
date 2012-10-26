7d5f851cdc2195d6c7a78c76a5732b5012186a08
Daniel Johnson <danjohns@mymail.mines.edu>
2012-10-25 21:47:27 -0600

    ch18 step 1,2, got key from google, added menu item for detail form, disable item if map is not saved

--- LunchList/src/csci498/danjohns/LunchList/DetailForm.java
+++ LunchList/src/csci498/danjohns/LunchList/DetailForm.java
@@ -12,8 +12,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -183,6 +181,7 @@ public class DetailForm extends Activity {
 	public boolean onPrepareOptionsMenu(Menu menu) {
 		if (restaurantID == null) {
 			menu.findItem(R.id.location).setEnabled(false);
+			menu.findItem(R.id.map).setEnabled(false);
 		}
 		
 		return super.onPrepareOptionsMenu(menu);
