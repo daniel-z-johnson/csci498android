5cb8a01b9095674c7071a54856a4b1f68ba631ee
Daniel Johnson <danjohns@mymail.mines.edu>
2012-10-26 22:53:31 -0600

    ch19 step3, add class onBootReceiver that extends BroadcastReceiver, added receiver permission for class

---
+++ LunchList/src/csci498/danjohns/LunchList/OnBootReceiver.java
@@ -0,0 +1,15 @@
+package csci498.danjohns.LunchList;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
