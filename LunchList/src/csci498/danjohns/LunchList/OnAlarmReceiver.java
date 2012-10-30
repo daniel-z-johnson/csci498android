0c024d9c60feba565bc1eadae686bc3b0c7d0a76
Daniel Johnson <danjohns@mymail.mines.edu>
2012-10-29 22:29:02 -0600

    ch20 step2, added code to onAlarmReceiver to enable a less disruptive notification

--- LunchList/src/csci498/danjohns/LunchList/OnAlarmReceiver.java
+++ LunchList/src/csci498/danjohns/LunchList/OnAlarmReceiver.java
@@ -1,18 +1,39 @@
 package csci498.danjohns.LunchList;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class OnAlarmReceiver extends BroadcastReceiver {
-
+	private static final int NOTIFY_ME_ID = 16*9*5*7*11*13*17*19; //Project Euler solution, why not?
+	
 	@Override
 	public void onReceive(Context ctxt, Intent intent) {
-		Intent i = new Intent(ctxt, AlarmActivity.class);
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctxt);
+		boolean useNotifications = prefs.getBoolean("use_notification", true);
 		
-		i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		
-		ctxt.startActivity(i);
+		if (useNotifications) {
+			NotificationManager mgr = (NotificationManager)ctxt.getSystemService(Context.NOTIFICATION_SERVICE);
+			
+			Notification note = new Notification(R.drawable.stat_notify_chat, "It's time for linch!", System.currentTimeMillis());
+			PendingIntent i = PendingIntent.getActivity(ctxt, 0, new Intent(ctxt, AlarmActivity.class), 0);
+			
+			note.setLatestEventInfo(ctxt, "LunchList", "It's time for lunch! Aren't you hungry?", i);
+			note.flags |= Notification.FLAG_AUTO_CANCEL;
+			
+			mgr.notify(NOTIFY_ME_ID, note);
+		} else {
+			Intent i = new Intent(ctxt, AlarmActivity.class);
+			
+			i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			
+			ctxt.startActivity(i);
+		}
 	}
 
 }
