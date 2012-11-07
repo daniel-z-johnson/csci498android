2860f0ce70d738ce300571adb8ea9411fa498f99
Daniel Johnson <danjohns@mymail.mines.edu>
2012-11-06 22:10:17 -0700

    ch22 step3, added onHCUpdate to LunchListWidget

--- LunchList/src/csci498/danjohns/LunchList/LunchListWidget.java
+++ LunchList/src/csci498/danjohns/LunchList/LunchListWidget.java
@@ -1,10 +1,13 @@
 package csci498.danjohns.LunchList;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
+import android.widget.RemoteViews;
 
 public class LunchListWidget extends AppWidgetProvider {
 	
@@ -15,6 +18,28 @@ public class LunchListWidget extends AppWidgetProvider {
 		} else {
 			ctxt.startService(new Intent(ctxt, WidgetService.class));
 		}
+	}
+	
+	public void onHCUpdate(Context ctxt, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		for(int i = 0; i < appWidgetIds.length; ++i) {
+			Intent svcIntent = new Intent(ctxt, ListWidgetService.class);
+			
+			svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+			svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
+			
+			RemoteViews widget =  new RemoteViews(ctxt.getPackageName(), R.layout.widget);
+			
+			widget.setRemoteAdapter(appWidgetIds[i], R.id.restaurants, svcIntent);
+			
+			Intent clickIntent = new Intent(ctxt, DetailForm.class);
+			PendingIntent clickPI = PendingIntent.getActivity(ctxt, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+			
+			widget.setPendingIntentTemplate(R.id.restaurants, clickPI);
+			
+			appWidgetManager.updateAppWidget(appWidgetIds[i], widget);
+		}
+		
+		super.onUpdate(ctxt, appWidgetManager, appWidgetIds);
 	}
 	
 }
