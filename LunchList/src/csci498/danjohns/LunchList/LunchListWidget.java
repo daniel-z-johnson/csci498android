b9adf2d7f78c5fc2fdba32c0b3eb374fdd418239
Daniel Johnson <danjohns@mymail.mines.edu>
2012-11-06 19:38:27 -0700

    ch21 step6, override onUpdate in LunchListWidget

--- LunchList/src/csci498/danjohns/LunchList/LunchListWidget.java
+++ LunchList/src/csci498/danjohns/LunchList/LunchListWidget.java
@@ -1,7 +1,15 @@
 package csci498.danjohns.LunchList;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
 
 public class LunchListWidget extends AppWidgetProvider {
+	
+	@Override
+	public void onUpdate(Context ctxt, AppWidgetManager mgr, int[] appWidgetIds) {
+		ctxt.startService(new Intent(ctxt, WidgetService.class));
+	}
 	
 }
