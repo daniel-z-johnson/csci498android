1ccce342f6f6720cacaa27688589b669e0be5294
Daniel Johnson <danjohns@mymail.mines.edu>
2012-11-06 21:56:42 -0700

    ch22 step3, changed onUpdate in LunchListWidget

--- LunchList/src/csci498/danjohns/LunchList/LunchListWidget.java
+++ LunchList/src/csci498/danjohns/LunchList/LunchListWidget.java
@@ -4,12 +4,17 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 public class LunchListWidget extends AppWidgetProvider {
 	
 	@Override
 	public void onUpdate(Context ctxt, AppWidgetManager mgr, int[] appWidgetIds) {
-		ctxt.startService(new Intent(ctxt, WidgetService.class));
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			onHCUpdate(ctxt, mgr, appWidgetIds);
+		} else {
+			ctxt.startService(new Intent(ctxt, WidgetService.class));
+		}
 	}
 	
 }
