27a77d611338ad3a308da87c010c370621639312
Daniel Johnson <danjohns@mymail.mines.edu>
2012-11-13 20:04:06 -0700

    ch25 step2,created Help page activity, added HelpPage Activity to androidManifest

---
+++ LunchList/src/csci498/danjohns/LunchList/HelpPage.java
@@ -0,0 +1,19 @@
+package csci498.danjohns.LunchList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class HelpPage extends Activity {
+	private WebView browser;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help);
+		
+		browser = (WebView)findViewById(R.id.webkit);
+		browser.loadUrl("file:///android_asset/help.html");
+	}
+
+}
